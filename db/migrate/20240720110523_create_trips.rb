b1a5d752f701e22d512a5811b3a53252a6dacb0d
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-07-20 12:01:21 +0000

    Initial register

---
+++ db/migrate/20240720110523_create_trips.rb
@@ -0,0 +1,11 @@
+class CreateTrips < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trips do |t|
+
+      t.string :name
+      t.text :explain
+
+      t.timestamps
+    end
+  end
+end
